24b30d9b76ded16b2ea1e9c4a48ee82c4a0286e7
Rafi Mahardika <rafimahardika96@gmail.com>
2026-03-04 19:32:54 +0700

    add pagination warning for large accounts

--- app.py
+++ app.py
@@ -97,56 +97,60 @@ check_button = middle.button("Check now!", use_container_width=True)
 
 if check_button and username:
     max_pages_followers, max_pages_following, followers, following = asyncio.run(get_profile_data(username))
-    with st.status(f"Fetching account data for '{username}'...", expanded=True) as status:
-        with st.spinner("Fetching followers and following...", show_time=True):
-            followers_list, following_list = asyncio.run(main_async(username, max_pages_followers, max_pages_following))
 
-        status.update(label="Data loaded successfully!", state="complete", expanded=False)
-    
-    set_followers = set(followers_list)
-    set_following = set(following_list)
-
-    unfollowers = list(set_following - set_followers)
-    unfollowing = list(set_followers - set_following)
-
-    if followers != len(followers_list) or following != len(following_list):
-        st.warning("Letterboxd website might down or this website is blocked by Letterboxd. Please try again later.")
+    if max_pages_followers > 256 or max_pages_following > 256:
+        st.warning("This account has too many followers or following. The tool can only check up to 6400 followers/following due to Letterboxd's pagination limits.")
     else:
-        st.subheader(f"{username.title()}'s Profile:")
-        col1, col2 = st.columns(2)
-        with col1:
-            st.write(f"- 🚀 **Following**: {following}")
-        with col2:
-            st.write(f"- 🎟️ **Followers**: {followers}")
-        col3, col4 = st.columns(2)
-        with col3:
-            st.write(f"- 😒 **Doesn't Follow You Back**: {len(unfollowers)}")
-        with col4:
-            st.write(f"- 💔 **You Don't Follow Back**: {len(unfollowing)}")
+        with st.status(f"Fetching account data for '{username}'...", expanded=True) as status:
+            with st.spinner("Fetching followers and following...", show_time=True):
+                followers_list, following_list = asyncio.run(main_async(username, max_pages_followers, max_pages_following))
 
-        st.divider()
-        col5, col6 = st.columns(2)
-        with col5:
-            st.subheader("Doesn't Follow You Back:")
-            if unfollowers:
-                st.caption("People you follow, but they don’t follow you")
-                for i, user in enumerate(unfollowers, start=1):
-                    st.write(f"{i}. {user} : https://letterboxd.com/{user}/")
-            elif not followers and not following:
-                st.warning("You don't follow anyone and no one follows you.")
-            else:
-                st.success("Everyone you follow also follows you back! 🎉")
+            status.update(label="Data loaded successfully!", state="complete", expanded=False)
+        
+        set_followers = set(followers_list)
+        set_following = set(following_list)
 
-        with col6:
-            st.subheader("You Don't Follow Back:")
-            if unfollowing:
-                st.caption("People who follow you, but you don’t follow them")
-                for i, user in enumerate(unfollowing, start=1):
-                    st.write(f"{i}. {user} : https://letterboxd.com/{user}/")
-            elif not followers and not following:
-                st.warning("You don't follow anyone and no one follows you.")
-            else:
-                st.success("You follow back everyone who follows you! 👍")
+        unfollowers = list(set_following - set_followers)
+        unfollowing = list(set_followers - set_following)
+
+        if followers != len(followers_list) or following != len(following_list):
+            st.warning("Letterboxd website might down or this website is blocked by Letterboxd. Please try again later.")
+        else:
+            st.subheader(f"{username.title()}'s Profile:")
+            col1, col2 = st.columns(2)
+            with col1:
+                st.write(f"- 🚀 **Following**: {following}")
+            with col2:
+                st.write(f"- 🎟️ **Followers**: {followers}")
+            col3, col4 = st.columns(2)
+            with col3:
+                st.write(f"- 😒 **Doesn't Follow You Back**: {len(unfollowers)}")
+            with col4:
+                st.write(f"- 💔 **You Don't Follow Back**: {len(unfollowing)}")
+
+            st.divider()
+            col5, col6 = st.columns(2)
+            with col5:
+                st.subheader("Doesn't Follow You Back:")
+                if unfollowers:
+                    st.caption("People you follow, but they don’t follow you")
+                    for i, user in enumerate(unfollowers, start=1):
+                        st.write(f"{i}. {user} : https://letterboxd.com/{user}/")
+                elif not followers and not following:
+                    st.warning("You don't follow anyone and no one follows you.")
+                else:
+                    st.success("Everyone you follow also follows you back! 🎉")
+
+            with col6:
+                st.subheader("You Don't Follow Back:")
+                if unfollowing:
+                    st.caption("People who follow you, but you don’t follow them")
+                    for i, user in enumerate(unfollowing, start=1):
+                        st.write(f"{i}. {user} : https://letterboxd.com/{user}/")
+                elif not followers and not following:
+                    st.warning("You don't follow anyone and no one follows you.")
+                else:
+                    st.success("You follow back everyone who follows you! 👍")
 
 st.divider()
 st.markdown("🐞 Found a bug? Contact me — Made by [rafilajhh](https://letterboxd.com/rafilajhh/)")
